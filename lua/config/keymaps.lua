8d6f94962efd48874b781ae7a97f9a63cd9dd220
Qui Chương <quichuong2005@gmail.com>
2025-08-30 16:57:57 +0700

    config keymap.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,8 @@
-vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open Parent Directory in Oil" })
+-- Oil
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open parent directory" })
+
+-- Git
+vim.keymap.set("n", "<leader>gs", "<cmd>Git<CR>", { desc = "Git status" })
+
+-- Buffers
+vim.keymap.set("n", "<leader>bd", "<cmd>bd<CR>", { desc = "Delete buffer" })
